10919298319682647fbcba0fbc90a4127d89717c
Hugo Cowan <hugocowan@hotmail.co.uk>
2018-05-31 15:09:07 +0100

    Fixed dburi reference for heroku

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,6 @@
 const port = process.env.PORT || 4000;
 const env = process.env.NODE_ENV || 'dev';
-const dbURI = process.env.MONGODBURI || `mongodb://localhost/pcs-${env}`;
+const dbURI = process.env.MONGODB_URI || `mongodb://localhost/pcs-${env}`;
 const secret = process.env.SECRET || 'vhfebzcv';
 
 module.exports = {
